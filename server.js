ca994d0a52293b51277b47779a9f3210b490b9b4
Dale Systems <admin@dalesystems.com>
2017-03-19 12:16:57 +0530

    tes sd fsd

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     res.end('Jackle');
-=======
-    res.end('Hello, world! - Jackle');
->>>>>>> 63b77b9ed1954a63b23d482090b93101cb175474
-=======
-    res.end('Hello, world! - Jackle');
->>>>>>> 63b77b9ed1954a63b23d482090b93101cb175474
+
     
 }).listen(process.env.PORT || 8080);
